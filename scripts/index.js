cde285cf0aeaa63025e1494cb2c03d9f326ebbed
olga <memento1991@rambler.ru>
2022-07-11 23:36:06 +0800

    name job saving bugfix

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,13 @@ const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = document.querySelector('.popup__close');
 
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
-console.log(popupOpenButtonElement);
+
+let formElement = document.querySelector('.popup__form'); 
+
+const submitButtonElement = document.querySelector('.popup__save-button');
+
+const finalName = document.querySelector('.profile__name');
+const finalJob = document.querySelector('.profile__description');
 
 const openPopup = function() {
   popupElement.classList.remove('popup_is-closed');
@@ -16,22 +22,13 @@ const closePopup = function() {
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupOpenButtonElement.addEventListener('click', openPopup);
 
-
-let formElement = document.querySelector('.popup__form'); 
-
-const submitButtonElement = document.querySelector('.popup__save-button');
-
-
-
 function formSubmitHandler(evt) {
     evt.preventDefault(); 
+    let nameInput = document.querySelector("input[name='name']").value;
     let jobInput = document.querySelector("input[name='profession']").value;
 
-    const finalName = document.querySelector('.profile__name');
-    const finalJob = document.querySelector('.profile__description');
-    
-    finalName.textContent=nameInput;
-    finalJob.textContent =jobInput;
+    finalName.textContent = nameInput;
+    finalJob.textContent = jobInput;
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
